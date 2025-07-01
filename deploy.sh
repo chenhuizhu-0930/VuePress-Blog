52caa606c8ffa3bc9fc9d9ac4d964c8553724332
Cathy Chen <catchen@abeam.com>
2025-07-01 16:51:00 +0800

    更新文档内容 2025-07-01 16:51:00

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,19 @@ set -e
 push_addr="https://github.com/chenhuizhu-0930/VuePress-Blog.git" # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=main # 推送的分支
+src_branch=main # 源分支
+site_branch=gh-pages # 推送的分支
 
-# 0. 提交 Markdown 源文件变更（显示上次更新时间用）
-git add docs/
-git commit -m "更新文档内容 $(date '+%Y-%m-%d %H:%M:%S')" || echo "无文档变更，无需提交"
+# 推送源码到main
+git add . # 把修改放入暂存区（staging area）
+
+# 提交 Markdown 源文件变更（显示上次更新时间用）
+git commit -m "更新文档内容 $(date '+%Y-%m-%d %H:%M:%S')" || echo "没有文档变更，无需提交" # 提交到本地仓库
 echo "✅ Markdown 文件提交完毕"
+
+# 推送到远程仓库
+git push -u origin $src_branch 
+echo "✅ 源码已推送到 $src_branch"
 
 # 生成静态文件 windows的话这里用build:win
 npm run build:win 
@@ -21,10 +28,11 @@ echo "✅ 构建完成，生成新 dist"
 cd "$dist_path"
 
 git init
+git checkout -b gh-pages
 git add -A
-git commit -m "deploy, $测试上次更新时间"
-git push -f $push_addr HEAD:$push_branch
-echo "✅ 推送完成！dist 已部署到 $push_branch 分支"
+git commit -m "deploy, site: rebuild $(date '+%Y-%m-%d %H:%M:%S')"
+git push -f $push_addr HEAD:$site_branch
+echo "✅ 推送完成！dist 已部署到 $site_branch 分支"
 
 cd -
 rm -rf $dist_path
