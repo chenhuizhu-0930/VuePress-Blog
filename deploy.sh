614ac9794fe7f9e2be773e6f4adf9321470d93b3
Cathy Chen <catchen@abeam.com>
2025-09-19 16:45:03 +0800

    deploy 源码推送:更新概念区分博客 2025-09-19 16:45:03

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ site_branch=gh-pages # 推送的分支
 git add . # 把修改放入暂存区（staging area）
 
 # 提交 Markdown 源文件变更（显示上次更新时间用） 这里修改commit备注！
-git commit -m "deploy 源码推送:更新微软生态博客 $(date '+%Y-%m-%d %H:%M:%S')" || echo "没有文档变更，无需提交" # 提交到本地仓库
+git commit -m "deploy 源码推送:更新概念区分博客 $(date '+%Y-%m-%d %H:%M:%S')" || echo "没有文档变更，无需提交" # 提交到本地仓库
 echo "✅ Markdown 文件提交完毕"
 
 # 推送到远程仓库
@@ -31,7 +31,7 @@ git init
 git checkout -b gh-pages
 git add -A
 # 这里也要修改commit备注！
-git commit -m "更新微软生态博客 $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "更新概念区分博客 $(date '+%Y-%m-%d %H:%M:%S')"
 git push -f $push_addr HEAD:$site_branch
 echo "✅ 推送完成！dist 已部署到 $site_branch 分支"
 
